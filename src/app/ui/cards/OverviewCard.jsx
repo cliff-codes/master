5918c95aed063a359e95e78171d01af2ebeae738
Simple Codes <simplecodes2580@gmail.com>
2024-01-05 09:54:19 -0800

    overview card link route revoked

--- src/app/ui/cards/OverviewCard.jsx
+++ src/app/ui/cards/OverviewCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const OverviewCard = ({count, name}) => {
   return (
-   <Link href={"/"} className='cursor-pointer  rounded-sm bg-slate-600 text-slate-50 transition-all 100 ease-in'>
+   <Link href={""} className='cursor-pointer  rounded-sm bg-slate-600 text-slate-50 transition-all 100 ease-in'>
         <div className='h-40 flex flex-col place-items-center justify-center gap-4 py-4'>
             <div className='text-lg xs:text-2xl font-semibold'>{count}</div>
             <div className='text-xs xs:text-base sm:text-base text-clip'>{name}</div>
