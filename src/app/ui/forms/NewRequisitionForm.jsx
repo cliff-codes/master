a6d9f953a6e974be9b9219b1f81eafd743aa653b
cliff-codes <108398436+cliff-codes@users.noreply.github.com>
2024-01-13 14:22:40 -0800

    request new form ui built

--- src/app/ui/forms/NewRequisitionForm.jsx
+++ src/app/ui/forms/NewRequisitionForm.jsx
@@ -4,119 +4,146 @@ const NewRequisitionForm = () => {
   return (
     <div className='w-full '>
             <h1 className='bg-slate-700 text-slate-50 py-3 px-5 rounded-sm'>New Requisition Form</h1>
-        <div className='w-full flex flex-col sm:flex-row place-items-center mt-4'>
-            <form className='px-3 flex flex-col gap-4'>
-                <div className='flex place-items-center gap-3 mt-1'>
-                    <div className='w-32'>Req No:</div>
-                    <input type="text" placeholder="12321232" className="input input-bordered w-full max-w-xs" disabled = {true} />
-                </div>
+        <div className='w-full'>
+            <form className='px-3 mt-4 lg:flex gap-20'>
+               {/* Breaking the form items into two sections */}
+                <section className='flex flex-1 flex-col gap-4'>
+                    <div className='w-full flex place-items-center gap-3 mt-1 '>
+                        <div className='w-32'>Req No:</div>
+                        <input type="text" placeholder="12321232" className="input input-bordered w-full max-w-sm" disabled = {true} />
+                    </div>
 
-                <div className='flex place-items-center gap-3 mt-1'>
-                    <div className='w-32'>Requestor:</div>
-                    <input type="text" placeholder="12321232" className="input input-bordered w-full max-w-xs" disabled = {true} />
-                </div>
+                    <div className='w-full flex place-items-center gap-3 mt-1 '>
+                        <div className='w-32'>Requestor:</div>
+                        <input type="text" placeholder="12321232" className="input input-bordered w-full max-w-sm" disabled = {true} />
+                    </div>
 
-                <div className='flex place-items-center gap-3 mt-1'>
-                    <div className='w-32'>Financial Year</div>
-                    <input type="text" placeholder="12321232" className="input input-bordered w-full max-w-xs" disabled = {true} />
-                </div>
+                    <div className='w-full flex place-items-center gap-3 mt-1 '>
+                        <div className='w-32'>Financial Year</div>
+                        <input type="text" placeholder="12321232" className="input input-bordered w-full max-w-sm" disabled = {true} />
+                    </div>
 
-                <div className='flex place-items-center gap-3 mt-1'>
-                    <div className='w-32'>Requisition Type</div>
-                    <select className='max-w-xs w-full rounded-md'>
-                        <option>Purchase</option>
-                    </select>
-                </div>
+                    <div className='w-full flex place-items-center gap-3 mt-1 '>
+                        <div className='w-32'>Requisition Type</div>
+                        <select className='max-w-sm w-full rounded-md'>
+                            <option>Purchase</option>
+                        </select>
+                    </div>
 
-                <div className='flex place-items-center gap-3 mt-1'>
-                    <div className='w-32'>Location</div>
-                    <select className='max-w-xs w-full rounded-md'>
-                        <option>Head Office</option>
-                    </select>
-                </div>
+                    <div className='w-full flex place-items-center gap-3 mt-1 '>
+                        <div className='w-32'>Location</div>
+                        <select className='max-w-sm w-full rounded-md'>
+                            <option>Head Office</option>
+                        </select>
+                    </div>
 
-                <div className='flex place-items-center gap-3 mt-1'>
-                    <div className='w-32'>Currency</div>
-                    <select className='max-w-xs w-full rounded-md'>
-                        <option>GHC</option>
-                    </select>
-                </div>
+                    <div className='w-full flex place-items-center gap-3 mt-1 '>
+                        <div className='w-32'>Currency</div>
+                        <select className='max-w-sm w-full rounded-md'>
+                            <option>GHC</option>
+                        </select>
+                    </div>
 
-                <div className='flex place-items-center gap-3 mt-1'>
-                    <div className='w-32'>Priority</div>
-                    <select className='max-w-xs w-full rounded-md'>
-                        <option>Normal</option>
-                        <option>Urgent</option>
-                    </select>
-                </div>
+                    <div className='w-full flex place-items-center gap-3 mt-1 '>
+                        <div className='w-32'>Priority</div>
+                        <select className='max-w-sm w-full rounded-md'>
+                            <option>Normal</option>
+                            <option>Urgent</option>
+                        </select>
+                    </div>
 
-                <div className='flex place-items-center gap-3 mt-1'>
-                    <div className='w-32'>Budget Head</div>
-                    <select className='max-w-xs w-full rounded-md' defaultValue={'Select'}>
-                        <option>Normal</option>
-                        <option>Urgent</option>
-                    </select>
-                </div>
+                    <div className='w-full flex place-items-center gap-3 mt-1 '>
+                        <div className='w-32'>Budget Head</div>
+                        <select className='max-w-sm w-full rounded-md' defaultValue={'Select'}>
+                            <option>Normal</option>
+                            <option>Urgent</option>
+                        </select>
+                    </div>
 
-                <div className='flex place-items-center gap-3 mt-1'>
-                    <div className='w-32'>Budget Id</div>
-                    <input type="text" placeholder="budget id" className="input input-bordered w-full max-w-xs focus:outline-none focus:border-slate-800"  />
-                </div>
+                    <div className='w-full flex place-items-center gap-3 mt-1 '>
+                        <div className='w-32'>Budget Id</div>
+                        <input type="text" placeholder="budget id" className="input input-bordered w-full max-w-sm focus:outline-none focus:border-slate-800"  />
+                    </div>
 
-                <div className='flex place-items-center gap-3 mt-1'>
-                    <div className='w-32'>Budget Total</div>
-                    <input type="text" placeholder="0" className="input input-bordered w-full max-w-xs focus:outline-none focus:border-slate-800"  />
-                </div>
+                    <div className='w-full flex place-items-center gap-3 mt-1 '>
+                        <div className='w-32'>Budget Total</div>
+                        <input type="text" placeholder="0" className="input input-bordered w-full max-w-sm focus:outline-none focus:border-slate-800"  />
+                    </div>
 
-                <div className='flex place-items-center gap-3 mt-1'>
-                    <div className='w-32'>Expenditure Type</div>
-                    <input type="text" placeholder="0" className="input input-bordered w-full max-w-xs focus:outline-none focus:border-slate-800"  />
-                </div>
+                    <div className='w-full flex place-items-center gap-3 mt-1 '>
+                        <div className='w-32'>Expenditure Type</div>
+                        <input type="text" placeholder="0" className="input input-bordered w-full max-w-sm focus:outline-none focus:border-slate-800"  />
+                    </div>
+                </section>
 
-                <div className='flex place-items-center gap-3 mt-1'>
-                    <div className='w-32'>Total Amount Spent</div>
-                    <input type="text" placeholder="0" className="input input-bordered w-full max-w-xs focus:outline-none focus:border-slate-800"  />
-                </div>
+                <section className='flex flex-col gap-4 mt-3 flex-1'>
 
-                <div className='flex place-items-center gap-3 mt-1'>
-                    <div className='w-32'>Available Balance</div>
-                    <input type="text" placeholder="0" className="input input-bordered w-full max-w-xs focus:outline-none focus:border-slate-800"/>
-                </div>
+                    <div className='w-full flex place-items-center gap-3 mt-1 '>
+                        <div className='w-32'>Total Amount Spent</div>
+                        <input type="text" placeholder="0" className="input input-bordered w-full max-w-sm focus:outline-none focus:border-slate-800"  />
+                    </div>
 
-                <div className='flex place-items-center gap-3 mt-1'>
-                    <div className='w-32'>General Purspose of Requisition</div>
-                    <textarea className="textarea textarea-bordered" placeholder="Bio"></textarea>
-                </div>
+                    <div className='w-full flex place-items-center gap-3 mt-1 '>
+                        <div className='w-32'>Available Balance</div>
+                        <input type="text" placeholder="0" className="input input-bordered w-full max-w-sm focus:outline-none focus:border-slate-800"/>
+                    </div>
 
-                <div className='flex place-items-center gap-3 mt-1'>
-                    <div className='w-32'>Requisition Total</div>
-                    <input type="text" placeholder="0" className="input input-bordered w-full max-w-xs focus:outline-none focus:border-slate-800"/>
-                </div>
+                    <div className='w-full flex place-items-center gap-3 mt-1 '>
+                        <div className='w-32'>General Purspose of Requisition</div>
+                        <textarea className="textarea textarea-bordered w-full max-w-sm h-32 focus:outline-none focus:border-slate-800" placeholder="Bio"></textarea>
+                    </div>
+
+                    <div className='w-full flex place-items-center gap-3 mt-1 '>
+                        <div className='w-32'>Requisition Total</div>
+                        <input type="text" placeholder="0" className="input input-bordered w-full max-w-sm focus:outline-none focus:border-slate-800"/>
+                    </div>
 
 
-                <div className='flex place-items-center gap-3 mt-1'>
-                    <div className='w-32'>Extra Budgetary</div>
-                    <input type="text" placeholder="0" className="input input-bordered w-full max-w-xs focus:outline-none focus:border-slate-800"/>
-                </div>
+                    <div className='w-full flex place-items-center gap-3 mt-1 '>
+                        <div className='w-32'>Extra Budgetary</div>
+                        <input type="text" placeholder="0" className="input input-bordered w-full max-w-sm focus:outline-none focus:border-slate-800"/>
+                    </div>
 
-                <div className='flex place-items-center gap-3 mt-1'>
-                    <h1>Actions: </h1>
-                    <div>
-                        <div className='flex place-items-center gap-2'>
-                            <input type='checkbox'/>
-                            <h4>Save Requisition</h4>
-                        </div>
-                        <div className='flex place-items-center gap-2'>
-                            <input type='checkbox'/>
-                            <h4>Send for Approval</h4>
+                    <div className='w-full flex place-items-center gap-3 mt-1 '>
+                        <h1>Actions: </h1>
+                        <div>
+                            <div className='flex place-items-center gap-2'>
+                                <input type='checkbox'/>
+                                <h4>Save Requisition</h4>
+                            </div>
+                            <div className='flex place-items-center gap-2'>
+                                <input type='checkbox'/>
+                                <h4>Send for Approval</h4>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </section>
             </form>
 
             {/* Add requisition Items form */}
-            <form>
-            
+            <form className='w-full px-3 mt-4 lg:flex gap-20'>
+                <div className='w-full'>
+                    <h1 className='bg-slate-700 text-slate-50  px-3 py-2 mt-4 rounded-md w-full'>Add Requisition Items</h1>
+                    
+                    <div className='mt-4 w-full'>
+                        <div className=''>Item Description*</div>
+                        <textarea className="textarea textarea-bordered w-full max-w-lg h-32 focus:outline-none focus:border-slate-800" placeholder="description"></textarea>
+                    </div>
+
+                    <div className='flex gap-10'>
+                        <div>
+                            <h4>Quantity</h4>
+                            <input type="text" placeholder="0" className="input input-bordered w-full max-w-sm focus:outline-none focus:border-slate-800"/>
+                        </div>
+
+                        <div>
+                            <h4>Rate*</h4>
+                            <input type="text" placeholder="0" className="input input-bordered w-full max-w-sm focus:outline-none focus:border-slate-800"/>
+                        </div>
+                    </div>
+
+                    <button className='btn w-full mt-4 btn-success text-slate-50'>Add Item</button>
+                </div>
             </form>
         </div>
     </div>
